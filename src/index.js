64a2abb72cfbd086614a2fa2b6fe78c7a21ecfd8
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-04-18 21:59:15 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const app =express();
 const {PORT}=require('./config/serverConfig');
-const {sendBasicEmail}=require('./services/email-service')
-
+// const {sendBasicEmail}=require('./services/email-service')
+const cron =require('node-cron');
 const setupAndStartServer=()=>
 {
 
@@ -14,13 +14,16 @@ app.listen(PORT,()=>
 {
 console.log(`Server started at port ${PORT}`);
 
-sendBasicEmail(
-    'yashasvikaaap.com',
-    'balwaniharsh1001@gmail.com',
-    'This is a testing mail',
-    'Hey,Just checking it is working fine or not'
+// sendBasicEmail(
+//     'support@gmail.com',
+//     'balwaniharsh1001@gmail.com',
+//     'This is a testing mail',
+//     'Hey,Just checking it is working fine or not'
 
-)
+// )
+cron.schedule('',()=>{
+    console.log("");
+});
 });
 }
 
